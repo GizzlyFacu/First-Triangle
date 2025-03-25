9695f6eaf8d4969ed3f2f96a34466dd07e1e58bf
GizzlyFacu <pamdulxe@gmail.com>
2025-03-25 05:12:59 -0300

    changes

--- camera.cpp
+++ camera.cpp
@@ -1,19 +1,22 @@
 #include "camera.h"
 #include <gtx/rotate_vector.hpp>
+#include <gtx/string_cast.hpp>
+
 #include <iostream>
 
 void camera::MoveForward(float speed) {
-	mEye.z -= speed;
+	mEye +=mViewDirection*speed;
+	
 }
 void camera::MoveBackward(float speed) {
-	mEye.z += speed;
+	mEye -= mViewDirection * speed;
 }
 void camera::MoveLatLeft(float speed) {
-	mEye.x -= speed;
+	mEye -= glm::cross((mViewDirection), mUpVector) * speed;
 	
 }
 void camera::MoveLatRigth(float speed) {
-	mEye.x += speed;
+	mEye += glm::cross((mViewDirection), mUpVector)*speed;
 	
 
 }
@@ -26,6 +29,6 @@ void camera::MouseMovement(float mouseX, float mouseY) {
 	std::cout << "mouse y: " << mouseY << "\n";
 	// si se aprita la d-a a la vez que se gira se modifica esto de manera trambolica
 	mViewDirection = glm::rotate(mViewDirection, glm::radians(mouseX),mUpVector);
-	mViewDirection = glm::rotate(mViewDirection, glm::radians(mouseY), glm::cross(mViewDirection,mUpVector));
+	mViewDirection = glm::rotate(mViewDirection, glm::radians(mouseY), glm::cross((mViewDirection),mUpVector));
 
 }
